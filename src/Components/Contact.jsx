8787d57a2504bcfbf4a3d87cd22b1965526b1975
Bibek <bistabibek9@gmail.com>
2024-12-19 15:02:45 +0545

    updates on contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import { FaMapLocation } from "react-icons/fa6";
-// import { IoCall } from "react-icons/io5";
-// import { IoIosMail } from "react-icons/io";
-// import { IoMdTimer } from "react-icons/io";
-
-
-
-
-
-// const Contact = () => {
-//   return (
-//     <div className='min-h-screen'>
-//       <p className='text-center mt-2 text-xl font-nav font-semibold'>Get In Touch</p>
-//       <div className='w-full px-8 py-4'>
-//         <h2 className='font-nav font-bold'>Visit Our Local Store</h2>
-//         <div className='mt-2 text-gray-600 space-y-3'>
-//           <p className='flex items-center'><FaMapLocation size={20} /><span className='text-black mx-5 font-mono'>Mid-Baneshwor, Kathmandu</span></p>
-//           <p className='flex items-center'><IoCall size={20} /><span className='text-black mx-5 font-mono'>01-417700</span></p>
-//           <p className='flex items-center'><IoIosMail size={20} /><span className='text-black mx-5 font-mono'>bistabibek9@gmail.com</span></p>
-//           <p className='flex items-center'><IoMdTimer size={20} /><span className='text-black mx-5 font-mono'>9:00 AM - 6:00 PM</span></p>
-//         </div>
-//       </div>
-//       {/* -----------------------Map Section------------------------- */}
-
-
-//     </div>
-//   )
-// }
-
-// export default Contact
-
-
-
-
 import React from 'react';
 import { FaMapLocation } from "react-icons/fa6";
 import { IoCall } from "react-icons/io5";
