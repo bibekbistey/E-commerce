9524e6ddaec8804a3af35266feb23d2ffc0cb027
Bibek <bistabibek9@gmail.com>
2025-01-16 17:30:44 +0545

    added map using iframes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -50,17 +50,7 @@ const Contact = () => {
       animate={{opacity:1}}
       transition={{duration:1,delay:0.9}}
       className="w-full px-10 py-4 z-0">
-        <MapContainer center={position} zoom={15} style={{ height: "400px", width: "100%" }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          <Marker position={position}>
-            <Popup>
-              Mid-Baneshwor, Kathmandu <br /> Visit us here!
-            </Popup>
-          </Marker>
-        </MapContainer>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3532.6135142328044!2d85.33793507492271!3d27.698337725889775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDQxJzU0LjAiTiA4NcKwMjAnMjUuOCJF!5e0!3m2!1sen!2snp!4v1737027675108!5m2!1sen!2snp" width="600" height="450" style={{ border: '0' }}  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </motion.div>
       </div>
     </div>
